79b13c511d4ef4d5a326d3268e8de954e1563828
an.sajinsatheesan <an.sajinsatheesan@gmail.com>
2022-08-27 19:47:35 +0400

    create home page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
- 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-         Hello sajin
-        </header>
-      </div>
-    )
-  }
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";    
+import Home from './pages/home'
+
+const App = () => { 
+  return ( 
+      <BrowserRouter> 
+          <Routes>  
+              <Route exact path="/" name="Login Page" element={<Home />} /> 
+              {/* <Route exact path="/" name="Login Page" element={<Navigate to={'/login'} />} />
+              <Route path="/forgot-password" name="Forgot Page" element={<ForgotPassword />} />
+              <Route path="/404" name="Page 404" element={<Page404 />} /> */}
+          </Routes>
+      </BrowserRouter>
+  )
 }
 
-export default App
+
+export default App;
